c682b2b71122ff9f12997ad53a97af071a521eea
truco <trucopaulista23@gmail.com>
2020-05-30 13:33:03 -0300

    feat(add sonarqube): sonarqube

--- cypress/integration/header/header.spec.js
+++ cypress/integration/header/header.spec.js
@@ -1,18 +1,3 @@
 /// <reference types="cypress" />
 
-describe('Testando o header', () => {
-
-  beforeEach(() => {
-    cy.visit('/')
-  });
-
-  it('Verificar se existe o componente de header', () => {
-    cy.get('.header').should('to.have.length', 1)
-  });
-
-  it('Verificar se dentro do header possui o botão de SignUp', () => {
-    const btn = cy.get('.header-btn-signup');
-
-    expect(btn).to.exist;
-  })
-})
+describe('Page Component Header', () => { })
